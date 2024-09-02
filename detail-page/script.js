641fd6b9eddde5f1bc8a4c6a959c8089fb79a4cf
InGyo <zmfosqpfl101@gmail.com>
2024-09-02 17:48:12 +0900

    0902
    html도 css도 멀었다

---
+++ detail-page/script.js
@@ -0,0 +1,12 @@
+<script>
+  {/* $(document).ready(function() {
+    $('.car-image-slider.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+}); */}
+
+
+</script>
