a7d55716c6ed2b4609741305ffc9159256069f86
InGyo <zmfosqpfl101@gmail.com>
2024-09-10 16:51:46 +0900

    어~ 자바 시작이야~

---
+++ Chap03/src/TypeConversion01.java
@@ -0,0 +1,14 @@
+
+public class TypeConversion01 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 3.0;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x/y);
+		System.out.println(x*y);
+
+	}
+
+}
