3ef2873b0dd338ad764d616a77509f0c5ea18b95
InGyo <zmfosqpfl101@gmail.com>
2024-09-12 17:37:33 +0900

    0912 수업 커밋

--- BookMarket/src/welcome.java
+++ BookMarket/src/welcome.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class welcome {
+	static final int NUM_BOOK = 3;
+	static final int NUM_ITEM = 7;
 
 	public static void main(String[] args) {
+		
+		String[][]mBook = new String[NUM_BOOK][NUM_ITEM];
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -59,7 +63,7 @@ public class welcome {
 						break;
 					case 4:
 //						System.out.println("장바구니의 항목 추가하기 :");
-						menuAddCartItem();
+						menuCartAddItem(mBook);
 						break;
 					case 5:
 //						System.out.println("장바구니의 항목 수량 줄이기 :");
@@ -107,8 +111,47 @@ public class welcome {
 		System.out.println("3.장바구니 비우기");
 	}
 	
-	public static void menuAddCartItem () {
-		System.out.println("4.바구니에 항목 추가하기");
+	public static void menuCartAddItem (String[][] book) {
+		//System.out.println("4.바구니에 항목 추가하기");
+		BookList(book);
+		for (int i = 0; i < NUM_BOOK; i++) {
+			for (int j = 0; j < NUM_ITEM; j++)
+				System.out.print(book[i][j]+" | ");
+			System.out.println("");
+		}
+		
+		boolean quit  = false;
+		
+		while (!quit) {
+			System.out.println("장바구니에 추가할 도서의 ID를 입력하세요.");
+			
+			Scanner input = new Scanner(System.in);
+			String str = input.nextLine();
+			
+			boolean flag = false;
+			int numId = -1;
+			
+			for (int i = 0; i < NUM_BOOK; i++) {
+				if(str.equals(book[i][0])) {
+					numId = i;
+					flag = true;
+					break;
+				}
+			}
+			
+			if (flag) {
+				System.out.println("장바구니에 추가하겠습니까? Y | N");
+				str = input.nextLine();
+				
+				if (str.toUpperCase().equals("Y")) {
+					System.out.println(book[numId][0]+"도서가 장바구니에 추가되었습니다.");
+				}
+					quit = true;
+				}else {
+					System.out.println("다시 입력해 주세요");
+			}
+			
+			}
 	}
 	
 	public static void menuCartRemoveItemCount () {
@@ -125,6 +168,33 @@ public class welcome {
 	
 	public static void menuExit () {
 		System.out.println("8.종료");
+	}
+	
+	public static void BookList(String[][]book) {
+		
+		book[0][0] = "ISBN1234";
+		book[0][1] = "쉽게 배우는 JSP 웹 프로그래밍";
+		book[0][2] = "27000";
+		book[0][3] = "송미영";
+		book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
+		book[0][5] = "IT전문서";
+		book[0][6] = "2018/10/08";
+		
+		book[1][0] = "ISBN1235";
+		book[1][1] = "안드로이드 프로그래밍";
+		book[1][2] = "233000";
+		book[1][3] = "우재남";
+		book[1][4] = "실습 단계별 명확한 멘토링!";
+		book[1][5] = "IT전문서";
+		book[1][6] = "2022/01/22";
+	
+		book[2][0] = "ISBN1236";
+		book[2][1] = "스크래치";
+		book[2][2] = "223000";
+		book[2][3] = "고광일";
+		book[2][4] = "컴퓨터 사고력을 키우는 블록코딩";
+		book[2][5] = "컴퓨터 입문";
+		book[2][6] = "2019/06/10";
 	}
 
 }
---
+++ Chap07/src/Cat2.java
@@ -0,0 +1,7 @@
+
+class Cat2 extends Animal {
+
+	public void Sound () {
+		System.out.println("고양이는 야옹야옹");
+	}
+}
