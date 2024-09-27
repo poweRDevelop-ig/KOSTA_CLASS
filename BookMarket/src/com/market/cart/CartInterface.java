0ded613305c227b2efa4ed52d5195536df0756d1
InGyo <zmfosqpfl101@gmail.com>
2024-09-27 17:53:32 +0900

    0927 수업 커밋

--- BookMarket/src/com/market/cart/CartInterface.java
+++ BookMarket/src/com/market/cart/CartInterface.java
@@ -1,9 +1,10 @@
 package com.market.cart;
 
 import com.market.bookitem.Book;
+import java.util.ArrayList;
 
 public interface CartInterface {
-	void printBookList(Book[] p);
+	void printBookList(ArrayList<Book> p);
 	boolean isCartInBook(String id);
 	void insertBook(Book p);
 	void removeCart(int numId);
